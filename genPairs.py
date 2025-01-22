ee0ebec738deceaa38dcbf08d49a3c2daaa4ad66
Ke Chen <kxc5915@psu.edu>
2025-01-22 09:48:27 -0500

    genPairs do up to len number of mutations

--- genPairs.py
+++ genPairs.py
@@ -10,14 +10,14 @@ def kMutations(s:str, k:int) -> str:
     return t
 
 if len(sys.argv) != 2:
-    print('Usage: genPairs.py len\nGenerate 1M sequences of length len, randomly mutate each up to len/2 times to create 1M pairs. Compute the edit distance between each pair. Output to three files: s-len.fa, t-len.fa, and ed-len.npy. The corresponding entry in the three files make up a pair of sequences and their edit distance.')
+    print('Usage: genPairs.py len\nGenerate 1M sequences of length len, randomly mutate each up to len times to create 1M pairs. Compute the edit distance between each pair. Output to three files: s-len.fa, t-len.fa, and ed-len.npy. The corresponding entry in the three files make up a pair of sequences and their edit distance.')
     sys.exit(1)
 
-num_pairs = 1000000
+num_pairs = 100000
 length = int(sys.argv[1])
 
 s = [randSeq(length) for _ in range(num_pairs)]
-num_mut = np.random.randint(0, length//2+1, len(s))
+num_mut = np.random.randint(0, length+1, len(s))
 t = [kMutations(s[i], num_mut[i]) for i in range(len(s))]
 ed = np.array([Levenshtein.distance(s[i], t[i]) for i in range(len(s))])
 
